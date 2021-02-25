809ec28ac89d368da9c413b793288b6f0ccf6691
Eremi <Zyxel1995@mail.ru>
2021-02-25 11:50:22 +0300

    day2

---
+++ Backend/MFF_Base.py
@@ -0,0 +1,6 @@
+from Modules.Users import *
+
+
+user=User('Клуб')
+print(user)
+user.add_to_db()
--- Backend/Modules/Users.py
+++ Backend/Modules/Users.py
@@ -15,85 +15,134 @@ from configparser import ConfigParser
 from functools import wraps
 import mysql.connector
 from mysql.connector import Error
+from os import getcwd
 
-def read_db_config(filename='db_config.ini', section='mysql'):
-    """ Читает конфигурацию Базы данных и возвращает словарь с параметрами
-    :param filename: имя конфига
-    :param section: секция с данными базы данных
-    :return: словарь с параментрами
-    """
-    parser = ConfigParser()
-    parser.read(filename)
-    db = {}
-    if parser.has_section(section):
-        items = parser.items(section)
-        for item in items:
-            db[item[0]] = item[1]
-    else:
-        raise Exception('{0} not found in the {1} file'.format(section, filename))
+path_to_db = getcwd()[0:-7] + 'DB\\db_config.ini'
 
-    return db
+
+def read_db_config(filename=path_to_db, section='mysql'):
+	""" Читает конфигурацию Базы данных и возвращает словарь с параметрами
+	:param filename: имя конфига
+	:param section: секция с данными базы данных
+	:return: словарь с параментрами
+	"""
+	parser = ConfigParser()
+	parser.read(filename)
+	db = {}
+	if parser.has_section(section):
+		items = parser.items(section)
+		for item in items:
+			db[item[0]] = item[1]
+	else:
+		raise Exception('{0} not found in the {1} file'.format(section, filename))
+
+	return db
 
 
 def connect(func):
-    """Подключение в БД """
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        db_config = read_db_config()
+	"""Подключение в БД """
 
-        try:
-            print('Соединение с MySQL базой...')
-            global conn
-            conn = mysql.connector.connect(**db_config)
-            global cursor
-            cursor = conn.cursor()
-            if conn.is_connected():
-                print('соединение установлено.')
-            else:
-                print('соединения нет!!!.')
+	@wraps(func)
+	def wrapper(*args, **kwargs):
+		db_config = read_db_config()
 
-        except Error as error:
-            print(error)
-        func_result=func(*args, **kwargs)
-        if func_result:
-            conn.close()
-            print('Соединение закрыто.')
-            return func_result
-        else:
-            conn.close()
-            print('Соединение закрыто.')
+		try:
+			print('Соединение с MySQL базой...')
+			global conn
+			conn = mysql.connector.connect(**db_config)
+			global cursor
+			cursor = conn.cursor()
+			if conn.is_connected():
+				print('соединение установлено.')
+			else:
+				print('соединения нет!!!.')
 
-    return (wrapper)
+		except Error as error:
+			print(error)
+		func_result = func(*args, **kwargs)
+		if func_result:
+			conn.close()
+			print('Соединение закрыто.')
+			return func_result
+		else:
+			conn.close()
+			print('Соединение закрыто.')
+
+	return (wrapper)
 
 
 class User:
-	def __init__(self,name):
-		self.name=name
+
+	def __init__(self, name):
+		self.name = name
 
 	@connect
-	def add_to_db(self,table=users,columns='name',values={1:self.name}):
-    ''' Вставка значения'''
-    value_str=''
-    if len(values.keys())>1:
-        for i in list(values.keys())[0:-1]:
-            value_str=value_str+str(values[i])+','+'\n'
-        value_str=value_str+str(values[list(values.keys())[-1]])+';'
-    else:
-        value_str=value_str+f'({str(values[list(values.keys())[-1]])})'+';'
-    
-    if columns=='':
-    	query = f"INSERT INTO {table} VALUES {value_str}"
-    else:
-    	query = f"INSERT INTO {table}({columns}) VALUES {value_str}"
-    cursor.execute(query)
-    conn.commit()
+	def add_to_db(self, table='users', columns='name', values={}):
+		"""
+		Добавить юзера в БД
+		:param table:
+		:param columns:
+		:param value_str:
+		:return:
+		"""
+		if not values.keys:
+			values[1] = self.name
+		value_str = ''
+		if len(values.keys()) > 1:
+			for i in list(values.keys())[0:-1]:
+				value_str = value_str + str(values[i]) + ',' + '\n'
+			value_str = value_str + str(values[list(values.keys())[-1]]) + ';'
+		else:
+			value_str = value_str + f'({str(values[list(values.keys())[-1]])})' + ';'
 
-    @connect
-    def del_from_db(self,table=users,columns='name',values={1:self.name}):
-    	query = f"DELETE FROM {table} WHERE {columns} = {values[1]}"
-    	cursor = conn.cursor()
-	    cursor.execute(query, (book_id,))
-	    conn.commit()
+		if columns == '':
+			query = f"INSERT INTO {table} VALUES {value_str}"
+		else:
+			query = f"INSERT INTO {table}({columns}) VALUES {value_str}"
+		cursor.execute(query)
+		conn.commit()
+
+	@connect
+	def del_from_db(self, table='users', columns='name', values={}):
+		"""
+		Удалить юзера из БД
+		:param self:
+		:param table:
+		:param columns:
+		:param values:
+		:return:
+		"""
+		if not values.keys:
+			values[1] = self.name
+		query = f'DELETE FROM {table} WHERE {columns} = {values[1]}'
+		cursor = conn.cursor()
+		cursor.execute(query)
+		conn.commit()
+
+	@connect
+	def update_data_user(self, table='users', columns='name', values={}):
+		"""
+		Обновление данных о юзере
+		:param value_str: 
+		:param table: 
+		:param columns: 
+		:return: 
+		type value_str: dict
+		"""
+		if not values.keys:
+			values[1] = self.name
+		query = f"UPDATE {table} SET {columns} = {values[1]} WHERE {columns}={self.name}"
+		cursor.execute(query)
+		conn.commit()
 
 
-Функции изменить инфо в БД
+class Club(User):
+	pass
+
+
+class State(User):
+	pass
+
+
+class Stadium(User):
+	pass
